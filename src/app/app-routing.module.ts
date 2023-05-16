a7a721528448d3e7052a9e01796402a1037abea7
Savin <savinov.vladislav@gmail.com>
2023-05-16 12:59:15 +0300

    standalone routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './auth/auth.guard'
+
+const routes: Routes =[
+    { path: '', 
+	  loadChildren: () => import('./pages/start/start.module').then(m => m.StartModule),
+	  },
+    { path: 'settings',
+		loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule),
+		canLoad:[AuthGuard], canActivate:[AuthGuard]},
+    { path: 'content', 
+		loadChildren: () => import('./pages/fbi-content/fbi-content.module').then(m => m.FbiContentModule),
+		canLoad: [AuthGuard], canActivate: [AuthGuard]}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+	RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';  
-import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import {MaterialModule} from './material/material.module'
-import { StartComponent } from './pages/start/start.component'
-import { FbiContentComponent } from './pages/fbi-content/fbi-content.component'
-import { SettingsComponent } from './pages/settings/settings.component'
 import { SharedModule } from './shared/shared/shared.module';
-import {AuthGuard} from './auth/auth.guard'
+import {AppRoutingModule} from './app-routing.module';
 
-const appRoutes: Routes =[
-    { path: '', component: StartComponent},
-    { path: 'content', component: FbiContentComponent, canActivate:[AuthGuard]},
-    { path: 'settings', component: SettingsComponent, canActivate:[AuthGuard]}
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent,
-    FbiContentComponent,
-    SettingsComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
     BrowserAnimationsModule,
+	AppRoutingModule,
     HeaderModule,
     MaterialModule,
-    RouterModule.forRoot(appRoutes),
     SharedModule
   ],
   providers: [],
