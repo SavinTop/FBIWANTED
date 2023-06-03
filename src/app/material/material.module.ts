a5a702ee3644441fca57a47fcf3acc90b8a5a721
= <=>
2023-06-03 11:15:04 +0300

    new mat elements

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button'
-import {MatDialogModule} from '@angular/material/dialog'
-import {MatInputModule} from '@angular/material/input';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon'
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatTableModule} from '@angular/material/table'
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatStepperModule } from '@angular/material/stepper';
+
 
 @NgModule({
   declarations: [],
@@ -22,27 +27,35 @@ import {MatDividerModule} from '@angular/material/divider';
     MatDialogModule,
     MatInputModule,
     MatSidenavModule,
-	MatIconModule,
-	MatProgressSpinnerModule,
-	MatProgressBarModule,
-	MatTableModule,
-	MatPaginatorModule,
-	MatCardModule,
-	MatDividerModule
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatStepperModule
   ],
-  exports:[
-	MatToolbarModule,
+  exports: [
+    MatToolbarModule,
     MatButtonModule,
     MatDialogModule,
     MatInputModule,
     MatSidenavModule,
-	MatIconModule,
-	MatProgressSpinnerModule,
-	MatProgressBarModule,
-	MatTableModule,
-	MatPaginatorModule,
-	MatCardModule,
-	MatDividerModule
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatStepperModule
   ]
 })
 export class MaterialModule { }
