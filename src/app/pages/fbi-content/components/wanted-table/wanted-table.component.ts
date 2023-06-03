e00f93e705c8e66f0e4220bc14a03dafbdf70993
= <=>
2023-06-03 15:34:52 +0300

    edit buttons added

--- src/app/pages/fbi-content/components/wanted-table/wanted-table.component.ts
+++ src/app/pages/fbi-content/components/wanted-table/wanted-table.component.ts
@@ -4,6 +4,7 @@ import { FbiapiService } from '../../services/fbiapi.service';
 import { PageEvent } from '@angular/material/paginator';
 import {WantedList} from '../../models/wanted-list';
 import {WantedItem} from '../../models/wanted-item';
+import { EditedItem } from '../../models/edit-queue-item';
 
 @Component({
   selector: 'app-wanted-table',
@@ -12,10 +13,13 @@ import {WantedItem} from '../../models/wanted-item';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WantedTableComponent {
-	columnsToDisplay = ['title','sex', 'race','eyes','hair']
+	columnsToDisplay = ['title','sex', 'race','eyes','hair', 'edited']
 
 	@Input()
 	data: WantedList|null = null
+
+	@Input()
+	editedUids: Set<string> = new Set()
 
 	@Output()
 	onPerson = new EventEmitter<WantedItem>()
@@ -23,18 +27,23 @@ export class WantedTableComponent {
 	@Output()
 	onPageChange = new EventEmitter<{pageIndex:number, pageSize: number}>()
 
+	currentPageSize: number = 25
+
 	constructor(public api:FbiapiService,
 				public cd: ChangeDetectorRef
 			   ){
 		
 	}
 
-	handlePageEvent($event: PageEvent){
-		this.onPageChange.emit({pageIndex: $event.pageIndex+1, pageSize: $event.pageSize})
+	handlePageEvent(event: PageEvent){
+		this.currentPageSize = event.pageSize
+		this.onPageChange.emit({pageIndex: event.pageIndex+1, pageSize: event.pageSize})
 	}
 
 	handleRowClick($row: WantedItem){
 		this.onPerson.emit($row)
 	}
 
+
+	@Output() editedClicked = new EventEmitter()
 }
