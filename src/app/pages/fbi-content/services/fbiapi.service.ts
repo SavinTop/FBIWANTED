573662df1442f038bcdb997b481f3a324fb8a475
= <=>
2023-06-03 15:37:24 +0300

    mock methods for edited items

--- src/app/pages/fbi-content/services/fbiapi.service.ts
+++ src/app/pages/fbi-content/services/fbiapi.service.ts
@@ -11,14 +11,24 @@ export class FbiapiService  {
 
 	constructor(private http: HttpClient) { }
 
-	requestWantedList(page: number, pageSize: number){
+	requestWantedList(page: number, pageSize: number, title: string = ''){
 		return this.http.get<WantedList>("https://api.fbi.gov/wanted/v1/list", {
 			params: {
 				page,
-				pageSize
+				pageSize,
+				title
 			}
 		})
 
+	}
+
+	getEditQueueList() : Observable<WantedItem[]>{
+		return this.http.get<WantedItem[]>("https://647632afe607ba4797dd6fda.mockapi.io/wanted/mock/edit-queue", {
+		})
+	}
+
+	markForEdit(item:WantedItem){
+		return this.http.post<WantedItem>("https://647632afe607ba4797dd6fda.mockapi.io/wanted/mock/edit-queue", item)
 	}
 
 }
